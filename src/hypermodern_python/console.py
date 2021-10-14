b8d757db007a1875647b6946168dff68c8d0093a
holiveira <v-holiveira@hotels.com>
2021-10-14 21:03:41 +0200

    Update to catch api error

--- src/hypermodern_python/console.py
+++ src/hypermodern_python/console.py
@@ -12,12 +12,15 @@ API_URL = "https://en.wikipedia.org/api/rest_v1/page/random/summary"
 @click.version_option(version=__version__)
 def main():
     """The hypermodern Python project"""
-    with requests.get(API_URL) as response:
-        response.raise_for_status()
-        data = response.json()
+    try:
+        with requests.get(API_URL) as response:
+            response.raise_for_status()
+            data = response.json()
+        
+        title = data["title"]
+        extract = data["extract"]
 
-    title = data["title"]
-    extract = data["extract"]
-
-    click.secho(title, fg="green")
-    click.echo(textwrap.fill(extract))
+        click.secho(title, fg="green")
+        click.echo(textwrap.fill(extract))
+    except requests.HTTPError as e:
+        click.secho(f"API error: {e}", fg="red")
