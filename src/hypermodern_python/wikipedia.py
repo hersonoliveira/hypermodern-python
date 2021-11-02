9077ef852ea76e0a01bf030678d8542a75faad38
holiveira <v-holiveira@hotels.com>
2021-11-02 15:44:00 +0100

    Refactor console

---
+++ src/hypermodern_python/wikipedia.py
@@ -0,0 +1,15 @@
+import requests
+import click
+
+
+API_URL = "https://en.wikipedia.org/api/rest_v1/page/random/summary"
+
+
+def random_page():
+    try:    
+        with requests.get(API_URL) as response:
+            response.raise_for_status()
+            return response.json()
+    except requests.RequestException as e:
+        message = str(e)
+        raise click.ClickException(message)
