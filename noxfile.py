c7c80df57004bed411bd8edfef34f272d17c9cb8
holiveira <v-holiveira@hotels.com>
2021-11-05 12:01:40 +0100

    Add mypy check

--- noxfile.py
+++ noxfile.py
@@ -4,7 +4,7 @@ import nox
 
 
 locations = "src", "tests", "noxfile.py"
-nox.options.sessions = "lint", "safety", "tests"
+nox.options.sessions = "lint", "mypy", "tests"
 
 
 @nox.session(python=["3.8", "3.7"])
@@ -52,6 +52,13 @@ def safety(session):
         )
         install_with_constraints(session, "safety")
         session.run("safety", "check", f"--file={requirements.name}", "--full-report")
+
+
+@nox.session(python=["3.8", "3.7"])
+def mypy(session):
+    args = session.posargs or locations
+    install_with_constraints(session, "mypy")
+    session.run("mypy", "--install-types", "--non-interactive", *args)
 
 
 def install_with_constraints(session, *args, **kwargs):
